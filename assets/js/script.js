86f3601e5cc8c392a1239dc2fb7fd9deeb3a367f
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-09-07 16:27:08 -0400

    Added Questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,26 @@ var timeleft;
 var score = 0
 var Highscore = [];
 timeEl.innerText = 0;
+
+var questions = [
+    { question: "What does HTML stand for?",
+      answer: "3. HyperText Markup Language",
+      choices: [{choice: "1. HyperText Markup Link"}, {choice: "2. Hypertext Markup Lead"}, {choice: "3. HyperText Markup Language"}, {choice: "4. None of the Above"}]     
+    },
+    { question: "What does CSS stand for?",
+      answer: "1. Cascading Style Sheets",
+      choices: [{choice: "1. Cascading Style Sheets"}, {choice: "2. Color and Style Sheets"}, {choice: "3. Coloured Special Sheets"}, {choice: "4. None of the Above"}]     
+    },
+    { question: "What element changes the background color?",
+      answer: "2. background-color",
+      choices: [{choice: "1. color"}, {choice: "2. background-color"}, {choice: "3. Both A and B"}, {choice: "4. None of the above"}]     
+    },
+    { question: "Which are CSS position property values?",
+      answer: "4. All of the Above",
+      choices: [{choice: "1. static"}, {choice: "2. relative"}, {choice: "3. fixed"}, {choice: "4. All of the Above"}]     
+    },
+    { question: "Which keywords define a variable in Javascript?",
+      answer: "4. Both A and B",
+      choices: [{choice: "1. var"}, {choice: "2. let "}, {choice: "3. for"}, {choice: "4. Both A and B"}]     
+    },
+]
