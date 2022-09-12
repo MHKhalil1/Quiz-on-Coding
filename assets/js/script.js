9eb0924a321ab035238bbb9001e3ecbe9a1857df
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-09-12 06:53:56 -0400

    applying the hidden feature with a display feature to the quiz when starting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Variables
 var highscoreEl = document.getElementById("highscore")
 var timeEl = document.querySelector("#timer")
 var starterEl = document.getElementById("starter")
@@ -17,7 +18,7 @@ var score = 0
 var Highscore = [];
 var QuestionCatalog = 0
 var arrayShuffledQuestions
-
+// Questions being asked along with choices and answers
 var questions = [
     { question: "What does HTML stand for?",
       answer: "3. HyperText Markup Language",
@@ -40,54 +41,13 @@ var questions = [
       choices: [{choice: "1. var"}, {choice: "2. let "}, {choice: "3. for"}, {choice: "4. Both A and B"}]     
     },
 ];
-
+// This function allows the quiz screen to be displayed and to be hidden
 var beginQuiz = function() {
   starterEl.classList.add("hide");
   starterEl.classList.remove("show");
-  questionholderEl.classList.remove("hide");
   questionholderEl.classList.add("show");
-  arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
-  setTime()
-  setQuestion()
+  questionholderEl.classList.remove("hide");
+  arrayShuffledQuestions = questions.sort(() => Math.random() - 0.2)
+  putQuestion()
+  placeTime()
 }
-
-var setQuestion = function() {
-  resetAnswers()
-  displayQuestion(arrayShuffledQuestions[QuestionCatalog])
-}
-
-var showQuestion = function(_index) {
-  questionEl.innerText = _index.q
-  for (var i = 0; i < _index.choices.length; i++) {
-    var answerbtn = document.createElement("button")
-    answerbtn.innerText = _index.choices[i].choice
-    answerbtn.classList.add("button")
-    answerbtn.classList.add("ansbtn")
-    answerbtn.addEventListener("click", answerVerification)
-    answerbtnEl.appendChild(answerbtn)
-  }
-};
-
-var answerVerification = function(_event) {
-  var selectedanswer = _event.target
-  if (arrayShuffledQuestions[QuestionCatalog].a === selectedanswer.innerText){
-    answerCorrect()
-    score = score + 5
-  }
-
-  else {
-    answerWrong()
-    timeleft = timeleft - 2;
-  };
-
-  QuestionCatalog++
-    if (arrayShuffledQuestions.length > QuestionCatalog + 1) {
-      setQuestion()
-    }
-    else {
-      gameover = "true";
-      showScore();
-    }
-
-}
-startbtnEl.addEventListener("click", beginQuiz)
